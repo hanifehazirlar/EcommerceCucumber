c6fc5d4886552bf0ae49bf12b1d7a4667230537b
Hanife Hazırlar <hanifehzrlar@gmail.com>
2023-03-08 10:07:07 +0300

    08/03/23

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
 
         features = "src/test/resources/features",
         glue = "stepDefinitions",
-        tags = "@Register",
+        tags = "@TC11",
         dryRun = false
 
 )
--- src/test/java/stepDefinitions/RegisterStepDef.java
+++ src/test/java/stepDefinitions/RegisterStepDef.java
@@ -134,7 +134,7 @@ public class RegisterStepDef {
 
     @And("User enters valid confirm password")
     public void userEntersValidConfirmPassword() {
-        registerPage.comfirmPasswordBox.sendKeys(ConfigReader.getProperty("validPassword"));
+        registerPage.passwordBox.sendKeys(ConfigReader.getProperty("validPassword"));
     }
 
     @And("User clicks the register button")
